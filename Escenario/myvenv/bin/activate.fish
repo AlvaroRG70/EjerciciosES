c88e0ed609c5791e0343d4f861654575c03f9562
Alvaro Rodriguez <rgalvaro70@gmail.com>
2023-11-12 13:11:15 +0100

    ejercicio tags completado

--- Escenario/myvenv/bin/activate.fish
+++ Escenario/myvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alumnado/Escritorio/2DAW_NUEVA/Servidor/Django/EjerciciosES/Escenario/myvenv"
+set -gx VIRTUAL_ENV "/home/alvaro/EjerciciosES/Escenario/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
